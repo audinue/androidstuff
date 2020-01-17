4c2a2e30c0491ca162897827f4be50abeeee93f8
Audi Nugraha <audinue@gmail.com>
2020-01-17 11:17:48 +0000

    Another awesome update

--- generate-debug-keystore.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-keytool -sigalg MD5withRSA -keyalg RSA -keysize 1024 -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -dname "CN=Android Debug,O=Android,C=US"
--- vars.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-set -e
-
-export ANDROID_JAR=/usr/lib/android-sdk/platforms/android-23/android.jar
-export UNSIGNED_APK=HelloWorld-unsigned.apk
-export UNALIGNED_APK=HelloWorld-unaligned.apk
-export FINAL_APK=HelloWorld.apk
